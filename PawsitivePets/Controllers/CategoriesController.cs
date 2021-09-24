205b571ca64dabb948cd0cf48513feadafd6d5c1
Preston Lacasse <76919751+prestonlacasse@users.noreply.github.com>
2021-09-24 09:34:53 -0400

    Lesson 2 | P3 - Display 10 mock categories using a model

--- PawsitivePets/Controllers/CategoriesController.cs
+++ PawsitivePets/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PawsitivePets.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,31 @@ namespace PawsitivePets.Controllers
     {
         public IActionResult Index()
         {
+            // use the Category model to generate a list of 10 mock category objects
+            var Categories = new List<Category>();
+
+            for (var i = 1; i < 11; i++)
+            {
+                Categories.Add(new Category { CategoryId = i, Name = "Category" + i.ToString() });
+            }
+
+            //pass the categories list to the view for display
+            return View(Categories);
+        }
+
+        //add a new method that's called when user clicks a category from the list displayed
+        //method accepts a string value in the url called "category"
+        public IActionResult Browse(string category)
+        {
+            //If category is empty / missing, take user back to the category list -> Prevents user from seeing an uncomplete/missing page
+            if (category == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            //Store the input parameter in the ViewBag to display in the view
+            ViewBag.category = category;
+
             return View();
         }
     }
---
+++ PawsitivePets/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawsitivePets.Models
+{
+    public class Category
+    {
+        //in .net, for fields, use the name (Model)Id or just Id
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
